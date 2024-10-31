0cc6750a46b4cffc8b52324236a154f0adb2bd7c
BEKSULTAN <230599@astanait.edu.kz>
2024-10-31 18:56:17 +0500

    UserAdded

--- src/main/java/com/kz/OnlineQuiz/OnlineQuizApplication.java
+++ src/main/java/com/kz/OnlineQuiz/OnlineQuizApplication.java
@@ -8,6 +8,7 @@ public class OnlineQuizApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineQuizApplication.class, args);
+
 	}
 
 }
---
+++ src/main/java/com/kz/OnlineQuiz/model/User.java
@@ -0,0 +1,54 @@
+package com.kz.OnlineQuiz.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userID;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    public Long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
